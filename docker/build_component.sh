9fd3b3e4fcf499378f5893ec63db176d9575a584
majik <me@yamajik.com>
2018-11-22 10:10:03 +0800

    fix docker

--- docker/build_component.sh
+++ docker/build_component.sh
@@ -5,9 +5,11 @@ set -xe
 IMAGE_NAME=kaggle_no1_component
 IMAGE=registry.cn-shanghai.aliyuncs.com/shuzhi/${IMAGE_NAME}
 
-docker build -t ${IMAGE}:$1 . -f ./docker/Dockerfile_Component_$1
+docker build -t ${IMAGE}:$1-$2 . -f ./docker/Dockerfile_Component_$1
+
 docker tag ${IMAGE}:$1-$2 ${IMAGE}:$1
 docker tag ${IMAGE}:$1-$2 ${IMAGE}:latest
+
 docker push ${IMAGE}:$1-$2
 docker push ${IMAGE}:$1
 docker push ${IMAGE}:latest
